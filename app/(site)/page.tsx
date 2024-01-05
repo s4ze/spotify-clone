26447e86a9213246803bfbd391d535f69218ed03
s4ze <imgood1104@gmail.com>
2024-01-05 21:02:21 +0500

    Добавление Supabase

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -45,8 +45,7 @@ export default function Home() {
         className="mt-2 mb-7 px-6"
       >
         <div
-          className="flex justify-between items-center"
-        >
+          className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">
             Newest Songs
           </h1>
