6992cd9abac249139b70504f5483e0cc3ff1752e
s4ze <imgood1104@gmail.com>
2024-01-05 17:52:10 +0500

    Добавление титульника, кнопок вернуться и вперед, вход, регистрация

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <div className="
@@ -7,9 +9,33 @@ export default function Home() {
       w-full
       overflow-hidden
       overflow-y-auto
-      "
-    >
+    ">
+      <Header>
+        <div className="mb-2">
+          <h1
+            className="
+              text-white
+              text-3xl
+              font-semibold
+            "
+          >
+            Welcome back
+          </h1>
+          <div
+            className="
+              grid
+              grid-cols-1
+              sm:grid-cols-2
+              xl:grid-cols-3
+              2xl:grid-cols-4
+              gap-3
+              mt-4
+            "
+          >
 
+          </div>
+        </div>
+      </Header>
     </div>
   )
 }
