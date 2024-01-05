2d76ce1e43676dbec4015f6be4d3e3d843fdd162
s4ze <imgood1104@gmail.com>
2024-01-05 19:38:25 +0500

    Добавление лицензии, кнопки "Понравившееся" и ListItem

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -32,10 +33,28 @@ export default function Home() {
               mt-4
             "
           >
-
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div
+        className="mt-2 mb-7 px-6"
+      >
+        <div
+          className="flex justify-between items-center"
+        >
+          <h1 className="text-white text-2xl font-semibold">
+            Newest Songs
+          </h1>
+        </div>
+        <div>
+          List of Songs
+        </div>
+      </div>
     </div>
   )
 }
